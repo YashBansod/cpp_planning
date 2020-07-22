e179cfc36b851e16e6aac09b45a8a8e35c7dc837
Yash Bansod <theyashbansod@gmail.com>
2020-07-21 21:24:34 -0400

    added kalman filter 2d example

---
+++ synthetic_data/traj_2d_monte_carlo.h
@@ -0,0 +1,27 @@
+/*!
+ * @brief
+ *
+ * @file
+ *
+ * @ingroup     synthetic_data
+ */
+
+/*------------------------------------------        Include Files           ------------------------------------------*/
+#include "Eigen/Dense"
+
+/*------------------------------------------        Using Statements      `------------------------------------------*/
+using namespace std;
+using Eigen::Matrix;
+using Eigen::MatrixXf;
+
+/*------------------------------------------        Class Declaration       ------------------------------------------*/
+#ifndef CPP_ROBOTICS_TRAJ_2D_MONTE_CARLO_H
+#define CPP_ROBOTICS_TRAJ_2D_MONTE_CARLO_H
+
+
+class traj_2d_monte_carlo {
+
+};
+
+
+#endif //CPP_ROBOTICS_TRAJ_2D_MONTE_CARLO_H
